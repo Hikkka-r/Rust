e42f512beb096a695783bbece47b87a68a13b551
songyuxin <2125563577@qq.com>
2021-03-21 12:10:15 +0800

    111

--- src/main/java/com/rust/Controller/HomeController.java
+++ src/main/java/com/rust/Controller/HomeController.java
@@ -3,7 +3,6 @@ package com.rust.Controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.rust.Item.Testitem;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,9 +11,9 @@ import java.util.ArrayList;
 public class HomeController {
     @GetMapping("/test")
     public String testOne(){
-        ArrayList<Testitem> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i=0;i<10;i++){
-            list.add(new Testitem(i,"just do it!"));
+            list.add(i);
         }
         return JSONObject.toJSONString(list);
     }
